a86938f8a385767a82f3c2b095ba50f1e5f89284
Jake Ihasz <ljakeail@gmail.com>
2018-04-25 15:23:00 -0400

    update

---
+++ update-repo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "APT Repository Packages Creation/Update Script"
+echo "Created by EthanRDoesMC"
+
+echo "Would you like to remove the current Packages file(s)?"
+select yn in "Yes" "No"; do
+case $yn in
+Yes ) rm Packages; rm Packages.gz; rm Packages.bz2; break;;
+No ) mv Packages OldPackages; mv Packages.gz OldPackages.gz; mv Packages.bz2 OldPackages.bz2; break;;
+esac
+done
+
+
+dpkg-scanpackages debs > Packages
+
+
+echo "Would you like to convert the newly created Packages file into gzip/bzip2?"
+select yn in "Yes" "No"; do
+case $yn in
+Yes ) gzip -c9 Packages > Packages.gz; bzip2 -c9 Packages > Packages.bz2; break;;
+No ) break;;
+esac
+done
+
+echo "Done!"
+exit
